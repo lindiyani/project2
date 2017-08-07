d9a899ca6492a7cbef557d9cfa8d5e2e54e52609
user <151610262@smkassalaambandung.sch.id>
2017-08-07 17:19:45 +0700

    sistem info

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h1>Sistem informasi sekolah</h1></div>
+
+                <div class="panel-body">
+                    selamat datang di aplikasi kami [*.*]
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
